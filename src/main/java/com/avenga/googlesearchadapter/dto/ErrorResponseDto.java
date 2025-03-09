7463ac9ec3bf3ce8b65e8aabd0d64d7dde95ec7f
Wojciech Misiaszek <wojciech.misiaszek@avenga.dev>
2025-03-09 20:22:20 +0100

    fixes after code review

---
+++ src/main/java/com/avenga/googlesearchadapter/dto/ErrorResponseDto.java
@@ -0,0 +1,16 @@
+package com.avenga.googlesearchadapter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponseDto {
+	
+   private String status;
+   private String description;
+}
